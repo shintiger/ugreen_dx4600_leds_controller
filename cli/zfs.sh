86a364f7b696287528f42e68735b2314470a7052
root <root@tnsbuilds01.tn.ixsystems.net>
2024-06-12 18:31:33 +0800

    -

---
+++ cli/zfs.sh
@@ -0,0 +1,82 @@
+#! /bin/bash
+
+#set -x
+
+SCRIPTPATH=$(dirname "$0")
+echo $SCRIPTPATH
+
+devices=(p n x x x x)
+map=(power netdev disk1 disk2 disk3 disk4)
+
+# Check network status
+gw=$(ip route | awk '/default/ { print $3 }')
+if ping -q -c 1 -W 1 $gw >/dev/null; then
+    devices[1]=u
+fi
+
+# Map sdX1 to hardware device
+declare -A hwmap
+echo "Mapping devices..."
+while read line; do
+    MAP=($line)
+    device=${MAP[0]}
+    hctl=${MAP[1]}
+    partitions=$(lsblk -l -o NAME | grep "^${device}[0-9]\+$")
+    for part in $partitions; do
+        hwmap[$part]=${hctl:0:1}
+        echo "Mapped $part to ${hctl:0:1}"
+    done
+done <<< "$(lsblk -S -o NAME,HCTL | tail -n +2)"
+
+# Print the hwmap for verification
+echo "Hardware mapping (hwmap):"
+for key in "${!hwmap[@]}"; do
+    echo "$key: ${hwmap[$key]}"
+done
+
+# Check status of zpool disks
+echo "Checking zpool status..."
+while read line; do
+    DEV=($line)
+    partition=${DEV[0]}
+    echo "Processing $partition with status ${DEV[1]}"
+    if [[ -n "${hwmap[$partition]}" ]]; then
+        index=$((${hwmap[$partition]} + 2))
+        echo "Device $partition maps to index $index"
+        if [ ${DEV[1]} = "ONLINE" ]; then
+            devices[$index]=o
+        else
+            devices[$index]=f
+        fi
+    else
+        echo "Warning: No mapping found for $partition"
+    fi
+done <<< "$(zpool status -L | grep -E '^\s+sd[a-h][0-9]')"
+
+# Output the final device statuses
+echo "Final device statuses:"
+
+while true; do
+    for i in "${!devices[@]}"; do
+        echo "$i: ${devices[$i]}"
+        case "${devices[$i]}" in
+            p)
+                "$SCRIPTPATH/ugreen_leds_cli" ${map[$i]} -color 255 255 255 -on -brightness 64
+                ;;
+            u)
+                "$SCRIPTPATH/ugreen_leds_cli" ${map[$i]} -color 255 255 255 -on -brightness 64
+                ;;
+            o)
+                "$SCRIPTPATH/ugreen_leds_cli" ${map[$i]} -color 0 255 0 -on -brightness 64
+                ;;
+            f)
+                "$SCRIPTPATH/ugreen_leds_cli" ${map[$i]} -color 255 0 0 -blink 100 200 -brightness 64
+                ;;
+            *)
+                "$SCRIPTPATH/ugreen_leds_cli" ${map[$i]} -off
+                ;;
+        esac
+    done
+
+    sleep 0.2
+done
